cb206c37ef20334c832c8cd8fd8cfba64dd9e4ec
MicaBittencourt <63079757+MicaBittencourt@users.noreply.github.com>
2021-07-18 22:57:47 -0300

    Alterações no código

--- src/Jokenpo.java
+++ src/Jokenpo.java
@@ -6,25 +6,50 @@ public class Jokenpo {
     public static void main(String[] args) {
 
         System.out.println("Digite sua jogada: ");
+        System.out.println("1 - Pedra\n" + "2 - Papel\n" + "3 - Tesoura");
 
-        int pedra = 1;
-        int papel = 2;
-        int tesoura = 3;
+        int jogador;
 
         Scanner jogadaDoUsuario = new Scanner(System.in);
         Random jogadaDoComputador = new Random();
 
-        int jogo1 = jogadaDoUsuario.nextInt();
+        jogador = jogadaDoUsuario.nextInt();
         int jogo2 = jogadaDoComputador.nextInt(3) + 1;
 
-        if (jogo1 == jogo2) {
+        switch (jogador){
+            case 1:
+                System.out.println("Você escolheu: Pedra");
+                break;
+            case 2:
+                System.out.println("Você escolheu: Papel");
+                break;
+            case 3:
+                System.out.println("Você escolheu: Tesoura");
+                break;
+        }
+
+        System.out.println("Jogada do Computador: " + jogo2);
+
+        switch (jogo2){
+            case 1:
+                System.out.println("Computador escolheu: Pedra");
+                break;
+            case 2:
+                System.out.println("Computador escolheu: Papel");
+                break;
+            case 3:
+                System.out.println("Computador escolheu: Tesoura");
+                break;
+        }
+
+        if (jogador == jogo2) {
             System.out.println("Houve um empate!!");
 
-        } else if ((jogo1 == pedra && jogo2 == tesoura) || (jogo1 == tesoura && jogo2 == papel) || (jogo1 == papel && jogo2 == pedra)) {
+        } else if ((jogador == 1 && jogo2 == 3) || (jogador == 3 && jogo2 == 2) || (jogador == 2 && jogo2 == 1)) {
             System.out.println("Você venceu!!");
 
-        } else if (jogo1 >= 4) {
-            System.out.println("Jogada Inválida!!");
+        } else if (jogador >= 4) {
+            System.out.println("Você fez uma jogada Inválida!!");
 
         } else { System.out.println("Computador venceu!!");
 
